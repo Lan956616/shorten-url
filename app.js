7aee611fb28d07ec1c580910780268fbafe494d4
Lan956616 <cd25924218@gmail.com>
2024-04-18 13:35:47 +0800

    setting express-handlebars

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const {engine} = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
 
+app.engine('.hbs', engine({extname:'.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
 app.get('/', (req,res)=>{
-  res.send('123')
+  res.render('index')
 })
 
 app.listen(port, ()=>{
