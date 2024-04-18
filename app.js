3e6dd316717975c9d505389ee6f0a5443dda9d28
Lan956616 <cd25924218@gmail.com>
2024-04-18 13:58:06 +0800

    using static file, setting index page content and css

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
 
-
+//引用靜態檔案
+app.use(express.static('public'))
+//設定樣版引擎
 app.engine('.hbs', engine({extname:'.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
+//設定路由
 app.get('/', (req,res)=>{
   res.render('index')
 })
 
+
+//監聽伺服器
 app.listen(port, ()=>{
   console.log(`express server is running on http://localhost:${port}`)
 })
